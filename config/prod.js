2a225e2f6095228e627f0acf6ba7b59cf4c076f3
Thong Pham <phminhthong88@gmail.com>
2021-09-04 00:42:00 -0700

    add google auth

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// config for production
+module.exports = {
+    googleClientId: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoUri: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+}
